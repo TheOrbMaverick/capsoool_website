13fcaf7eeb0490a1a591b88e7985981440e8d5d1
TheOrbMaverick <enangita@gmail.com>
2025-09-06 17:45:39 +0100

    env changes

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -175,7 +175,7 @@ export default function Contact() {
                     ref={captchaRef}
                     sitekey={
                       process.env.NEXT_PUBLIC_GOOGLE_CAPTCHA_SITE_KEY! ||
-                      "6LcjSL4rAAAAALYU4sCCKyumHuDno4Z8Xws5ryma"
+                      "6Lf8XcArAAAAALoudH00tJ1MMYEb828aA5zm1QB6"
                     }
                     onChange={onCaptchaChange}
                   />
