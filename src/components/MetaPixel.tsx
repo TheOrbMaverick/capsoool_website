4f4bfe4b0bdad333592e2e2c983f1a4f52a295a2
jeremiah-x <jeremiahhussain904@gmail.com>
2026-08-23 20:15:56 +0100

    meta pixel helper

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -8,7 +8,7 @@ export default function MetaPixel() {
     <>
       <Script
         id="meta-pixel"
-        // strategy="afterInteractive"
+        strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
           __html: `
 !function(f,b,e,v,n,t,s)
